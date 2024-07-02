50e5833de76d036a46845f65fa52be3ed6c3c182
Abhishek kumar <112263396+Abhicoder983@users.noreply.github.com>
2024-07-02 13:52:24 +0530

    Delete backend/LanguageProject directory

--- backend/LanguageProject/Languageapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class student(models.Model):
-    UserNames = models.CharField(max_length=100, default='null')
-    emails = models.CharField(max_length=100, default='null') 
-    password =models.CharField(max_length=50 , default='null') 
-
